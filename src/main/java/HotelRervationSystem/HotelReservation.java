0d3deae4b3a3988d481a3866aea3cbb20d63a6e3
riya40 <priyanunna20@gmail.com>
2021-10-26 09:19:23 +0530

    Added:Welcome Message

---
+++ src/main/java/HotelRervationSystem/HotelReservation.java
@@ -0,0 +1,12 @@
+package HotelRervationSystem;
+
+public class HotelReservation {
+    public static void main(String[] args) {
+        //Displaying the Welcome Message
+        System.out.println("Welcome to Hotel Reservation System");
+        HotelReservation hotelreservation=new HotelReservation();
+        
+    }
+
+
+}
