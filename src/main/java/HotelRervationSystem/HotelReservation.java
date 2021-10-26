29a5d8e9199e70ef17c494bf02ef6d031a734b40
riya40 <priyanunna20@gmail.com>
2021-10-26 09:25:53 +0530

    Added:Hotel Details

--- src/main/java/HotelRervationSystem/HotelReservation.java
+++ src/main/java/HotelRervationSystem/HotelReservation.java
@@ -1,11 +1,39 @@
 package HotelRervationSystem;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class HotelReservation {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void hoteldetails() {
+        int i;
+        String hotel, ratesforregular, rewardcustomer, o;
+        ArrayList<String> list = new ArrayList<String>();
+        for(i=0;i<3;i++)
+        {
+            System.out.println("enter hotel name");
+            hotel = scanner.nextLine();
+            System.out.println("enter the rates for for customer");
+            ratesforregular = scanner.next();
+            System.out.println("enter the reward customer");
+            rewardcustomer = scanner.next();
+
+            list.add("hotel name:" + hotel);
+            list.add("rates:" + ratesforregular);
+            list.add("rewardcustomer" + rewardcustomer);
+        }
+
+        System.out.println(list);
+    }
+
+
     public static void main(String[] args) {
         //Displaying the Welcome Message
         System.out.println("Welcome to Hotel Reservation System");
         HotelReservation hotelreservation=new HotelReservation();
-        
+        hotelreservation.hoteldetails();
+
     }
 
 
